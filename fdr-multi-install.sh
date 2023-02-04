cb48b59eb86522ad31f0f8a04b859e63ae751576
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2023-02-04 18:32:24 +0100

    Update fdr-multi-install.sh

--- fdr-multi-install.sh
+++ fdr-multi-install.sh
@@ -7,7 +7,7 @@ CONFIGFOLDER='/home/$USER/.fdreserve'
 COIN_PATH='/usr/local/bin/'
 #64 bit only
 COIN_TGZ='https://github.com/fdreserve/fdr-blockchain/releases/download/V2.3.0/2021-03-15_FDReserve_V230_Linux64.zip'
-BOOTSTRAP_TGZ='https://fdreserve.com/downloads/snapshot.zip'
+BOOTSTRAP_TGZ='https://downloads.fdreserve.com/bootstraps/fdr/snapshot.zip'
 COIN_DAEMON="fdreserved"
 COIN_CLI="fdreserve-cli"
 COIN_NAME='FDReserve'
@@ -312,7 +312,6 @@ function setup_node() {
 clear
 cd ~/
 detect_ubuntu
-create_swap
 prepare_system_for_download
 setup_node
 cd ~/
